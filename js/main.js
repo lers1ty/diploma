149af1580dabbce79915ad30e31dd600f79064e6
P1 <pashashruboks@yandex.ru>
2023-04-09 23:49:35 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+$('.header__row').slick({
+  responsive: [
+    {
+      breakpoint: 2048,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 870,
+      settings: {
+        arrows: false,
+        draggable: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        arrows: false,
+        draggable: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 $('.reviews__slider').slick({
   infinite: false,
   slidesToShow: 3,
